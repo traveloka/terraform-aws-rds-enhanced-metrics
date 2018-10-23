c549e70db330f6baa8ca0f6668e7ad6908724f4a
Febry Antonius <febry.antonius@traveloka.com>
2018-10-23 19:33:08 +0700

    Update several attribute value and change it using local instead

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "datadog_lambda_role" {
 
   product_domain   = "${var.product_domain}"
   service_name     = "${var.service_name}"
-  descriptive_name = "RDS enhanced metrics integration to Datadog"
+  descriptive_name = "${local.role_descriptive_name}"
 }
 
 resource "aws_iam_role_policy" "allow_decrypt_using_kms" {
@@ -19,19 +19,19 @@ resource "aws_iam_role_policy_attachment" "datadog_lambda_basic_execution" {
 module "lambda_function_name" {
   source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.7.1"
 
-  name_prefix   = "${var.product_domain}-send-enhanced-rds-to-datadog"
+  name_prefix   = "${var.service_name}-${local.role_descriptive_name}"
   resource_type = "lambda_function"
 }
 
-resource "aws_lambda_function" "send_enhanced_rds_to_datadog" {
+resource "aws_lambda_function" "send_rds_enhanced_to_datadog" {
   function_name    = "${module.lambda_function_name.name}"
   role             = "${module.datadog_lambda_role.role_arn}"
-  filename         = ".terraform/generated/lambda_function.zip"
-  source_code_hash = "${base64sha256(file(".terraform/generated/lambda_function.zip"))}"
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python2.7"
-  memory_size      = 128
-  timeout          = 10
+  filename         = "${data.archive_file.lambda_code.output_path}"
+  source_code_hash = "${data.archive_file.lambda_code.output_base64sha256}"
+  handler          = "${local.lambda_handler}"
+  runtime          = "${local.lambda_runtime}"
+  memory_size      = "${local.lambda_memory_size}"
+  timeout          = "${local.lambda_timeout}"
 
   environment {
     variables = {
@@ -52,7 +52,7 @@ resource "aws_lambda_function" "send_enhanced_rds_to_datadog" {
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id   = "AllowExecutionFromCloudWatch"
   action         = "lambda:InvokeFunction"
-  function_name  = "${aws_lambda_function.send_enhanced_rds_to_datadog.function_name}"
+  function_name  = "${aws_lambda_function.send_rds_enhanced_to_datadog.function_name}"
   principal      = "logs.${data.aws_region.current.name}.amazonaws.com"
   source_account = "${data.aws_caller_identity.current.account_id}"
   source_arn     = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:RDSOSMetrics:*"
@@ -62,5 +62,5 @@ resource "aws_cloudwatch_log_subscription_filter" "logfilter" {
   name            = "${var.service_name}_rdsDatadogIntegration_logfilter"
   log_group_name  = "RDSOSMetrics"
   filter_pattern  = ""
-  destination_arn = "${aws_lambda_function.send_enhanced_rds_to_datadog.arn}"
+  destination_arn = "${aws_lambda_function.send_rds_enhanced_to_datadog.arn}"
 }
